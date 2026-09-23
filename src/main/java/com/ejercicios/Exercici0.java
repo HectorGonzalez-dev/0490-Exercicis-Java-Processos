44f8e4487e7ce8ad39f79777c8301b10595d058e
HectorGonzalez-dev <hector.gonzalez.dev@gmail.com>
2026-09-23 17:33:59 +0200

    ejercicio renombrado

--- src/main/java/com/ejercicios/Exercici0.java
+++ src/main/java/com/ejercicios/Exercici0.java
@@ -1,10 +1,9 @@
 package com.ejercicios;
 
-/**
- * Hello world!
- */
-public class App {
+public class Exercici0 {
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
 }
